8c7021b57eced8e2630fc7e84000eccbbba57709
manigorman <immanakov@gmail.com>
2022-03-06 18:36:21 +0300

    Created Move object

--- TicTacToe/TicTacToe/ContentView.swift
+++ TicTacToe/TicTacToe/ContentView.swift
@@ -7,11 +7,14 @@
 
 import SwiftUI
 
-let columns: [GridItem] = [GridItem(.flexible()),
-                           GridItem(.flexible()),
-                           GridItem(.flexible())]
-
 struct ContentView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumanTurn = true
     
     var body: some View {
         GeometryReader { geometry in
@@ -26,18 +29,34 @@ struct ContentView: View {
                                 .frame(width: geometry.size.width / 3 - 15, height: geometry.size.width / 3 - 15)
                                 .shadow(radius: 5)
                             
-                            Image(systemName: "xmark")
+                            Image(systemName: moves[i]?.indicator ?? "")
                                 .resizable()
                                 .frame(width: 40, height: 40)
                                 .foregroundColor(.white)
                         }
+                        .onTapGesture {
+                            moves[i] = Move(player: isHumanTurn ? .human : .computer, boardIndex: i)
+                            isHumanTurn.toggle()
+                        }
                     }
+                    .padding()
                 }
-                .padding()
-                
                 Spacer()
             }
         }
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 
